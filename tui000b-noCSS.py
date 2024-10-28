c2d0d4543ea20cebf489e60c9cbc2d5d1c089b12
JD Shaffer <35787312+jdshaffer@users.noreply.github.com>
2024-10-29 07:36:53 +0900

    Preparing for my own tinkering

--- tui000b-noCSS.py
+++ tui000b-noCSS.py
@@ -1,15 +1,11 @@
 ###############################################################################
-# This version works for Jds on his machine.
-# Worked with Claude Sonnete to get it working
-#
-# Most changes were made in how it works with textual
-# (which was throwing tons of errors for me about setters and attributes).
-#
-# Also changed how the app was called at the very bottom, which seems to now 
-# be in the format the textual prefers.
-# 
-# 2024-10-28
+# 2024-10-29
 # Jds
+#
+# Goal --> Remove the CSS stuff that Claude wrote, go back to Hunter's
+#          code for textual layout
+#
+#
 ###############################################################################
 
 import shutil
